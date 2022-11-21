dde02420fdcb5aaddba59b50c69cc46ee6b93562
Arjen Andringa <aandringa@quintor.nl>
2022-11-21 17:47:47 +0100

    Acceptance!

--- energydb.sql
+++ energydb.sql
@@ -4,3 +4,10 @@ CREATE TABLE energydb(
     value VARCHAR(50) NOT NULL,
     PRIMARY KEY(created_at,value))
     PARTITION BY RANGE (created_at);
+
+CREATE TABLE acc_energydb(
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    sensor TEXT NOT NULL,
+    value VARCHAR(50) NOT NULL,
+    PRIMARY KEY(created_at,value))
+    PARTITION BY RANGE (created_at);
